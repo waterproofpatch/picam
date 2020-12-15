cb9dabf76806aa22a1b2ce4da940aeeba4946684
waterproofpatch <waterproofpatch@gmail.com>
2020-12-15 01:42:30 -0500

    flask db stuff

--- ipme/application.py
+++ ipme/application.py
@@ -1,8 +1,39 @@
 # standard imports
 import os
+from datetime import datetime
 
 # custom imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+db = SQLAlchemy(application)
+api = Api(application)
+
+
+class Ip(db.Model):
+    """
+    An IP address
+    """
+
+    __tablename__ = "ip"
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String, unique=False, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def as_json(self):
+        """
+        JSON representation of this model
+        """
+        payload = {
+            "id": self.id,
+            "ip": self.ip,
+        }
+
+        return payload
 
 
 def get_db_details():
@@ -19,7 +50,6 @@ def get_db_details():
 # print a nice greeting.
 def say_hello(username="World"):
     get_db_details()
-    payload = f"os.environ['RDS_DB_NAME'], os.environ['RDS_DB_NAME']"
     return payload
 
 
@@ -33,9 +63,6 @@ instructions = """
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = "</body>\n</html>"
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
-
 # add a rule for the index page.
 application.add_url_rule(
     "/", "index", (lambda: header_text + say_hello() + instructions + footer_text)
@@ -48,6 +75,7 @@ application.add_url_rule(
     "hello",
     (lambda username: header_text + say_hello(username) + home_link + footer_text),
 )
+
 
 # run the app.
 if __name__ == "__main__":
