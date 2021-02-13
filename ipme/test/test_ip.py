aef074be0bfb965058ca12a3d97558aef93ebedf
waterproofpatch <waterproofpatch@gmail.com>
2021-02-13 16:34:40 -0500

    testing

--- ipme/test/test_ip.py
+++ ipme/test/test_ip.py
@@ -4,5 +4,12 @@ Test the IP endpoint
 import pytest
 
 
+def test_ip_post(client):
+    response = client.post("/ip", json={"ip": "1.2.3.4"})
+    assert 200 == response.status_code
+
+
 def test_ip_get(client):
-    assert 200 == client.get("/ip").status_code
+    response = client.get("/ip")
+    assert 200 == response.status_code
+    assert [] == response.json
